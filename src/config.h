c432b2fb5570316f0f5331b6ab34f8a1b8d321c6
Ente <68391969+EntenPaule@users.noreply.github.com>
2021-01-02 21:00:01 +0100

    update config.h

--- src/config.h
+++ src/config.h
@@ -6,11 +6,11 @@
 #define CONFIG
 
   #define LEDPIN D2
-  const char* SSID = ""; // wlan SSID
-  const char* WPWD = ""; // wlan password
-  const char* APIKEY = ""; // octoprint api key (optain it in the web interface)
-  String OCTOIP = "192.168.1.3"; // the ip address of octoprint
-  uint16_t pollInterval = 500; // ms, lower value will increase poll rate, but the animation time will limit the poll rate too
+  const char* SSID = "00000"; // wlan SSID
+  const char* WPWD = "0000000"; // wlan password
+  String moonraker= "192.168.2.26"; // the ip address of Moonraker
+  uint16_t pollInterval = 1000; // ms, lower value will increase poll rate, but the animation time will limit the poll rate too
   const uint8_t length = 1; // number of LEDs
 
 #endif
+
